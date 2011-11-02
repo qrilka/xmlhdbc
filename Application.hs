4abd88abd1205227fd58cfd3ea0fb5ea05808272
Kirill Zaborsky <qrilka@gmail.com>
2011-11-02 17:50:36 +0400

    Simple CRUD implemented

--- Application.hs
+++ Application.hs
@@ -1,18 +1,19 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuasiQuotes, TypeFamilies, OverloadedStrings #-}
-module Application    
+module Application
        ( withXmlHDBC
        , withDevelAppPort
        ) where
 
 import Yesod
+import Yesod.Static
 import Yesod.Default.Config (DefaultEnv)
 import Yesod.Logger (Logger)
 import Yesod.Default.Main (defaultRunner, defaultDevelApp)
 
 import Data.Dynamic (Dynamic, toDyn)
-import Data.Text (Text, unpack)
+import Data.Text (unpack)
 
 import Data.Pool
 import qualified Database.HDBC as H
@@ -20,25 +21,33 @@ import qualified Database.HDBC.PostgreSQL as H
 
 import Control.Exception.Control (onException)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Arrow ((***))
 
 import Db
 
-
 data XmlHDBC = XmlHDBC {
-  settings     :: AppConfig DefaultEnv
+  getStatic    :: Static
+  , settings   :: AppConfig DefaultEnv
   , getLogger  :: Logger
   , getPool    :: Pool H.Connection
   , getContext :: Context
   }
 
+staticFiles "static"
+
 mkYesod "XmlHDBC" [parseRoutes|
+/static StaticR Static getStatic
 / HomeR GET
-/list/#Text ListTableR GET
+/list/#String ListR GET
+/create/#String CreateR POST
+/delete/#String DeleteR POST
+/update/#String UpdateR POST
 |]
 
 instance Yesod XmlHDBC where
   approot _ = ""
-  
+
+
 inDbPool :: Pool H.Connection -> (H.Connection -> IO a) -> IO a
 inDbPool pool f =
   withPool' pool $ \conn -> do
@@ -47,28 +56,95 @@ inDbPool pool f =
     H.commit conn
     return result
 
-inXmlTxn :: (MonadIO m, Functor m) => 
+inXmlTxn :: (MonadIO m, Functor m) =>
             (H.Connection -> Context -> IO a) -> GGHandler sub XmlHDBC m a
 inXmlTxn f = do
   XmlHDBC{getPool=pool, getContext=ctx} <- getYesod
   liftIO $ inDbPool pool $ \conn -> f conn ctx
 
 getHomeR :: Handler RepHtml
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = defaultLayout $ do
+  addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
+  addScript $ StaticR jquery_form_js
+  toWidget [lucius| form { display: inline }|]
+  toWidget [julius|
+function refresh() {
+  $.get("@{ListR "test"}", function(data) {
+    $('#table').empty();
+    $(data).find('row').each(function() {
+      var id = $(this).find('id').text();
+      var name = $(this).find('name').text();
+      $('#table').append($('<div id="row'+id+'"><form action="@{UpdateR "test"}" method="post">'+
+        '<input type="hidden" name="id" value="'+id+'"/>'+
+        '<input type="text" name="name" value="'+name+'"/><input type="submit" value="!"/></form>'+
+        '<form action="@{DeleteR "test"}" method="post"><input value="X" type="submit" onclick="del('+id+')"/></form></div>'));
+      $('#row'+id+' form').ajaxForm(function() {
+        refresh();
+        alert('done!');
+      });
+    });
+  }, "xml");
+}
 
-getListTableR :: Text -> Handler RepXml
-getListTableR table = do
+function del(id) {
+  $.post("@{DeleteR "test"}", {id: id}, refresh);
+}
+
+$(function() {
+  $("#adder").ajaxForm(function() {
+    alert("here you go!");
+    refresh();
+  });
+  refresh();
+});
+|]
+  [whamlet|
+<p>Hello World!
+<form #adder action=@{CreateR "test"} method=post>
+  <input name=name type=text>
+  <input type=submit>
+<div #table>
+|]
+
+getListR :: String -> Handler RepXml
+getListR table = do
   x <- inXmlTxn $ \conn ctx ->
-    doSelectFromTable conn ctx (unpack table) []
-  return $ RepXml $ toContent x --("<foo>" ++ (show x) ++ "</foo>")
+    doSelectFromTable conn ctx table []
+  return $ RepXml $ toContent x
+
+type DbOp a = H.Connection -> Context -> String -> [(String, String)] -> IO a
+
+postHandler :: String -> DbOp a -> Handler a
+postHandler table f = do
+  (params, _files) <- runRequestBody
+  inXmlTxn $ \conn ctx -> do
+    let unpacked = map (unpack *** unpack) params
+        tableParams = filterParams ctx table unpacked
+    f conn ctx table tableParams
+
+postCreateR :: String -> Handler RepXml
+postCreateR table = do
+  postHandler table doInsertIntoTable
+  return $ RepXml "<response/>"
+
+postUpdateR :: String -> Handler RepXml
+postUpdateR table = do
+  postHandler table doUpdateTable
+  return $ RepXml "<response/>"
+
+postDeleteR :: String -> Handler RepXml
+postDeleteR table = do
+  postHandler table doDeleteFromTable
+  return $ RepXml "<response/>"
 
 withXmlHDBC :: AppConfig DefaultEnv -> Logger -> (Application -> IO ()) -> IO ()
 withXmlHDBC conf logger f = do
-  let connect = H.connectPostgreSQL "dbname=rfid" 
+  s <- static "static"
+  let connect = H.connectPostgreSQL "dbname=rfid"
       disconnect = H.disconnect
   createPool connect disconnect 10 $ \p -> do
     ctx <- inDbPool p $ \conn -> newContext conn []
-    let h = XmlHDBC conf logger p ctx
+    let h = XmlHDBC s conf logger p ctx
     defaultRunner f h
 
 withDevelAppPort :: Dynamic
--- Db.hs
+++ Db.hs
@@ -14,6 +14,9 @@ data Context = Context {
   ctxKeys       :: M.Map String [String]
   }
 
+tableKeys :: Context -> String -> [String]
+tableKeys Context{ctxKeys=keys} table = fromMaybe [] $ M.lookup table keys
+
 newContext :: Connection -> [(String, [String])] -> IO Context
 newContext conn vk = do
   tables <- getTablesMeta conn
@@ -89,14 +92,68 @@ colsOfTable c t = do
 colsOfTables :: Connection -> [String] -> IO [(String, [String])]
 colsOfTables c = mapM (colsOfTable c)
 
+qparts :: String -> [(String, a)] -> (String, [a])
+qparts delim vals = 
+  let (s,b) = foldl (\(s',b') (k,v) -> ((k++" = ?"):s', v:b')) ([], []) vals 
+  in (intercalate delim $ reverse s, reverse b)
+
 xmlDoSelect :: Connection -> String -> [String] -> IO String
 xmlDoSelect conn query binds = do
     rs <- quickQuery' conn query (map tosql binds)
     return (intercalate "\n" $ t (xmlOfSet rs))
     where t x = ["<table>"] ++ x ++ ["</table>"]
 
+makeInsert :: String -> [(String, String)] -> (String, [String])
+makeInsert table values =
+    let cols = intercalate "," $ map fst values
+    in let holders = intercalate "," $ map (const "?") values 
+    in let binds = map snd values
+    in ("insert into " ++ table ++ "(" ++ cols ++ ")"  ++ " values (" ++ holders ++ ")" , binds)
+
+makeUpdate :: String -> [(String, String)] -> [String] -> (String, [String]) 
+makeUpdate table values keys =
+    let (keys', values') = partition (\(k,v) -> k `elem` keys) values
+    in let (sets, binds)     = qparts ", " values'
+    in let (clauses, binds') = qparts " and " keys'
+    in ("update " ++ table ++ " set " 
+                  ++ sets  ++ " where " 
+                  ++ ((show $ length keys')::String)
+                  ++ " > 0 and " ++ clauses, binds ++ binds')
+
+makeDelete :: String -> [(String, String)] -> [String] -> (String, [String]) 
+makeDelete table values keys =
+    let (keys', _) = partition (\(k,_) -> k `elem` keys) values
+    in let (clauses, binds') = qparts " and " keys'
+    in ("delete from " ++ table ++ " where " ++ ((show $ length keys')::String)
+                                ++ " > 0 and " ++ clauses, binds')
+filterParams :: Context ->  String -> [(String, String)] -> [(String, String)]
+filterParams ctx t p =
+    let static = [ "OFFSET", "LIMIT", "GUID" ]
+    in let cols = fromMaybe [] $ M.lookup t (ctxTables ctx)
+    in filter (\(k,_v) -> k `elem` (static ++ cols)) p
+
 doSelectFromTable :: Connection -> Context -> String -> [(String, String)] -> IO String
 doSelectFromTable conn ctx tbl args = do
     let cols = fromMaybe ["null as __fake"] $ M.lookup tbl (ctxTables ctx) 
     let (sql, binds) = xmlMakeSelectQuery tbl cols args
     xmlDoSelect conn sql binds
+
+doInsertIntoTable :: Connection -> Context -> String -> [(String, String)] -> IO ()
+doInsertIntoTable conn _ctx table args = do
+    let (sql, binds) = makeInsert table args
+    _ <- quickQuery' conn sql (map tosql binds)
+    return ()
+
+doUpdateTable :: Connection -> Context -> String -> [(String, String)] -> IO ()
+doUpdateTable conn ctx table values = do
+    let keys = tableKeys ctx table
+    let (sql, binds) = makeUpdate table values keys
+    _ <- quickQuery' conn sql (map tosql binds)
+    return ()
+
+doDeleteFromTable :: Connection -> Context -> String -> [(String, String)] -> IO ()
+doDeleteFromTable conn ctx table values = do
+    let keys = tableKeys ctx table
+    let (sql, binds) = makeDelete table values keys
+    _ <- quickQuery' conn sql (map tosql binds)
+    return ()
